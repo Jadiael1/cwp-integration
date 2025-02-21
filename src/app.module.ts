907ebacedf7e91c09fbcd92bddbfceebae99fa69
Jadiael <jadiael@hotmail.com.br>
2025-02-21 15:58:15 -0300

    feat(app): add EnvironmentConfigModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { UtilModule } from '@modules/util.module';
 import { CommonModule } from '@modules/common.module';
 import { RequestLoggerMiddleware } from '@middlewares/request-logger.middleware';
 import { AccountModule } from '@modules/account.module';
-
+import { EnvironmentConfigModule } from '@modules/environment-config.module';
 import { queueConfig, bullBoardConfig } from '@services/config.service';
 
 @Module({
-  imports: [bullBoardConfig, queueConfig, UtilModule, CommonModule, AccountModule],
+  imports: [EnvironmentConfigModule, bullBoardConfig, queueConfig, UtilModule, CommonModule, AccountModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {

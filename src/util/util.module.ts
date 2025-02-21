5cb305ef5e505a4f274ee2632ba9dcd2e619f465
Jadiael <jadiael@hotmail.com.br>
2025-02-21 15:59:32 -0300

    refactor(util): use ConfigService for Redis URL

--- src/util/util.module.ts
+++ src/util/util.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { UtilController } from '@controllers/util.controller';
 import { UtilService } from '@services/util.service';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    RedisModule.forRoot({
-      type: 'single',
-      url: process.env.NODE_ENV == 'production' ? process.env.REDIS_URL_PROD : process.env.REDIS_URL_DEV,
+    RedisModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.get<string>('REDIS_URL'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [UtilController],
